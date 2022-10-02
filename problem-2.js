b7c569f67f92c22d8285fb5b457865184ff51a45
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-02 10:30:09 +0300

    problem 2

---
+++ problem-2.js
@@ -0,0 +1,11 @@
+const JSX = (
+  <div>
+    <h1>Hello JSX!</h1>
+    <p>JSX is a syntax extension to JavaScript.</p>
+    <ul>
+      <li>JSX is a syntax extension to JavaScript.</li>
+      <li>JSX is a syntax extension to JavaScript.</li>
+      <li>JSX is a syntax extension to JavaScript.</li>
+    </ul>
+  </div>
+);
