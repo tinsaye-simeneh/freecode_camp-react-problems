f7cd4297f6e42cf6a5d0e401e233114cf28c15b9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-02 10:31:47 +0300

    problem 3

---
+++ problem-3.js
@@ -0,0 +1,13 @@
+const JSX = (
+    <div>
+        {/* This is a challenge 3 from freecodecamp problems */}
+      <h1>Hello JSX!</h1>
+      <p>JSX is a syntax extension to JavaScript.</p>
+      <ul>
+        <li>JSX is a syntax extension to JavaScript.</li>
+        <li>JSX is a syntax extension to JavaScript.</li>
+        <li>JSX is a syntax extension to JavaScript.</li>
+      </ul>
+    </div>
+  );
+  
